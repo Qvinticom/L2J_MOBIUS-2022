45f291118f3541967cdf671e72ba2a159d21994e
mobius <mobius@cyber-wizard.com>
2015-01-13 10:15:47 +0000

    Updating FlyToLocation packet.

--- trunk/java/com/l2jserver/gameserver/network/serverpackets/FlyToLocation.java
+++ trunk/java/com/l2jserver/gameserver/network/serverpackets/FlyToLocation.java
@@ -18,8 +18,8 @@
  */
 package com.l2jserver.gameserver.network.serverpackets;
 
-import com.l2jserver.gameserver.model.L2Object;
 import com.l2jserver.gameserver.model.actor.L2Character;
+import com.l2jserver.gameserver.model.interfaces.ILocational;
 
 /**
  * @author KenM
@@ -29,13 +29,22 @@ public final class FlyToLocation extends L2GameServerPacket
 	private final int _destX, _destY, _destZ;
 	private final int _chaObjId, _chaX, _chaY, _chaZ;
 	private final FlyType _type;
+	private int _flySpeed;
+	private int _flyDelay;
+	private int _animationSpeed;
 	
 	public enum FlyType
 	{
 		THROW_UP,
 		THROW_HORIZONTAL,
-		DUMMY, // no effect
-		CHARGE;
+		DUMMY,
+		CHARGE,
+		PUSH_HORIZONTAL,
+		JUMP_EFFECTED,
+		NOT_USED,
+		PUSH_DOWN_HORIZONTAL,
+		WARP_BACK,
+		WARP_FORWARD;
 	}
 	
 	public FlyToLocation(L2Character cha, int destX, int destY, int destZ, FlyType type)
@@ -50,9 +59,29 @@ public final class FlyToLocation extends L2GameServerPacket
 		_type = type;
 	}
 	
-	public FlyToLocation(L2Character cha, L2Object dest, FlyType type)
+	public FlyToLocation(L2Character cha, int destX, int destY, int destZ, FlyType type, int flySpeed, int flyDelay, int animationSpeed)
+	{
+		_chaObjId = cha.getObjectId();
+		_chaX = cha.getX();
+		_chaY = cha.getY();
+		_chaZ = cha.getZ();
+		_destX = destX;
+		_destY = destY;
+		_destZ = destZ;
+		_type = type;
+		_flySpeed = flySpeed;
+		_flyDelay = flyDelay;
+		_animationSpeed = animationSpeed;
+	}
+	
+	public FlyToLocation(L2Character cha, ILocational dest, FlyType type)
 	{
 		this(cha, dest.getX(), dest.getY(), dest.getZ(), type);
+	}
+	
+	public FlyToLocation(L2Character cha, ILocational dest, FlyType type, int flySpeed, int flyDelay, int animationSpeed)
+	{
+		this(cha, dest.getX(), dest.getY(), dest.getZ(), type, flySpeed, flyDelay, animationSpeed);
 	}
 	
 	@Override
@@ -67,5 +96,8 @@ public final class FlyToLocation extends L2GameServerPacket
 		writeD(_chaY);
 		writeD(_chaZ);
 		writeD(_type.ordinal());
+		writeD(_flySpeed);
+		writeD(_flyDelay);
+		writeD(_animationSpeed);
 	}
 }
