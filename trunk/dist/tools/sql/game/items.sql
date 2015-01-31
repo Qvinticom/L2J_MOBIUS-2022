dc99fb470c08ae46d764fd7d31374c1ac875c6f6
mobius <mobius@cyber-wizard.com>
2015-01-31 04:04:49 +0000

    Increased items table loc length to 13 characters.

--- trunk/dist/tools/sql/game/items.sql
+++ trunk/dist/tools/sql/game/items.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS `items` (
   `item_id` INT,
   `count` BIGINT UNSIGNED NOT NULL DEFAULT 0,
   `enchant_level` INT,
-  `loc` VARCHAR(10), -- inventory,paperdoll,npc,clan warehouse,pet,and so on
+  `loc` VARCHAR(13), -- inventory,paperdoll,npc,clan warehouse,pet,and so on
   `loc_data` INT,    -- depending on location: equiped slot,npc id,pet id,etc
   `time_of_use` INT, -- time of item use, for calculate of breackages
   `custom_type1` INT DEFAULT 0,
