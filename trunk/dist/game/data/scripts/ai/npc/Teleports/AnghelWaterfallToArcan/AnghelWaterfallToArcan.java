e382573b07e7e11680b515f80b50cd7d53d55f8f
mobius <mobius@cyber-wizard.com>
2015-02-23 11:21:13 +0000

    Anghel Waterfall Portal to Arcan.

---
+++ trunk/dist/game/data/scripts/ai/npc/Teleports/AnghelWaterfallToArcan/AnghelWaterfallToArcan.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2004-2015 L2J DataPack
+ * 
+ * This file is part of L2J DataPack.
+ * 
+ * L2J DataPack is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * L2J DataPack is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package ai.npc.Teleports.AnghelWaterfallToArcan;
+
+import ai.npc.AbstractNpcAI;
+
+import com.l2jserver.gameserver.model.Location;
+import com.l2jserver.gameserver.model.actor.L2Character;
+import com.l2jserver.gameserver.model.actor.instance.L2PcInstance;
+import com.l2jserver.gameserver.model.zone.L2ZoneType;
+
+/**
+ * Anghel Waterfall to Arcan teleport AI.
+ * @author Mobius
+ */
+public final class AnghelWaterfallToArcan extends AbstractNpcAI
+{
+	private static final int ZONE_ID = 200200;
+	private static final Location TELEPORT_LOC = new Location(207559, 86429, -1000);
+	
+	private AnghelWaterfallToArcan()
+	{
+		super(AnghelWaterfallToArcan.class.getSimpleName(), "ai/npc/Teleports");
+		addEnterZoneId(ZONE_ID);
+	}
+	
+	@Override
+	public String onEnterZone(L2Character character, L2ZoneType zone)
+	{
+		if (character.isPlayer())
+		{
+			final L2PcInstance player = character.getActingPlayer();
+			player.teleToLocation(TELEPORT_LOC);
+		}
+		return super.onEnterZone(character, zone);
+	}
+	
+	public static void main(String[] args)
+	{
+		new AnghelWaterfallToArcan();
+	}
+}
