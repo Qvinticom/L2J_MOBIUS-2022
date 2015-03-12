0b0385075a4436c2516c96534bb615943998c6b8
mobius <mobius@cyber-wizard.com>
2015-03-12 05:58:59 +0000

    Removed Jython parameter from startup files.

--- trunk/dist/game/GameServer_loop.sh
+++ trunk/dist/game/GameServer_loop.sh
@@ -7,7 +7,7 @@
 while :; do
 	[ -f log/java0.log.0 ] && mv log/java0.log.0 "log/`date +%Y-%m-%d_%H-%M-%S`_java.log"
 	[ -f log/stdout.log ] && mv log/stdout.log "log/`date +%Y-%m-%d_%H-%M-%S`_stdout.log"
-	java -Djava.util.logging.manager=com.l2jserver.util.L2LogManager -Dpython.cachedir=../cachedir -Xms1024m -Xmx1536m -jar l2jserver.jar > log/stdout.log 2>&1
+	java -Djava.util.logging.manager=com.l2jserver.util.L2LogManager -Xms1024m -Xmx1536m -jar l2jserver.jar > log/stdout.log 2>&1
 	[ $? -ne 2 ] && break
 #	/etc/init.d/mysql restart
 	sleep 10
