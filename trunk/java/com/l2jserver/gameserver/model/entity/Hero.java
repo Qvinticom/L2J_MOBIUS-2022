7a2b879901270649e049ea55856ed97fc87195bd
MobiusDev <MobiusDev@7325c9f8-25fd-504a-9f63-8876acdc129b>
2015-09-07 16:30:17 +0000

    Hero item fix. Contributed by Sahar.

--- trunk/java/com/l2jserver/gameserver/model/entity/Hero.java
+++ trunk/java/com/l2jserver/gameserver/model/entity/Hero.java
@@ -638,7 +638,7 @@ public class Hero
 			}
 			
 			final InventoryUpdate iu = new InventoryUpdate();
-			for (L2ItemInstance item : player.getInventory().getAvailableItems(false, false, false))
+			for (L2ItemInstance item : player.getInventory().getItems())
 			{
 				if ((item != null) && item.isHeroItem())
 				{
