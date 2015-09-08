0746a429b1c2ff219eb7dbc95de8ed6dc71cdba3
MobiusDev <MobiusDev@7325c9f8-25fd-504a-9f63-8876acdc129b>
2015-09-08 13:03:49 +0000

    Kekropus Letter (10390) rework. Contributed by spider.

--- trunk/dist/game/data/scripts/quests/Q10390_KekropusLetter/Q10390_KekropusLetter.java
+++ trunk/dist/game/data/scripts/quests/Q10390_KekropusLetter/Q10390_KekropusLetter.java
@@ -35,39 +35,64 @@ import com.l2jserver.gameserver.model.events.impl.character.player.OnPlayerLevel
 import com.l2jserver.gameserver.model.holders.ItemHolder;
 import com.l2jserver.gameserver.model.quest.Quest;
 import com.l2jserver.gameserver.model.quest.QuestState;
+import com.l2jserver.gameserver.model.quest.State;
 import com.l2jserver.gameserver.network.NpcStringId;
 import com.l2jserver.gameserver.network.serverpackets.ExShowScreenMessage;
 import com.l2jserver.gameserver.network.serverpackets.NpcHtmlMessage;
 
 /**
- * @author Neanrakyr
+ * Kekropus Letter (10390)
+ * @author Neanrakyr, spider
  */
 public class Q10390_KekropusLetter extends Quest implements IBypassHandler
 {
 	// NPCs
+	private static final int RAYMOND = 30289;
+	private static final int RAINS = 30288;
+	private static final int ELLENIA = 30155;
+	private static final int ESRANDELL = 30158;
+	private static final int TOBIAS = 30297;
 	private static final int DRIKUS = 30505;
+	private static final int MENDIO = 30504;
+	private static final int GERSHWIN = 32196;
 	private static final int BATHIS = 30332;
 	private static final int GOSTA = 30916;
 	private static final int ELI = 33858;
 	// Items
+	private static final ItemHolder KEKROPUS_LETTER = new ItemHolder(36706, 1);
+	private static final ItemHolder SCROLL_OF_ESCAPE_HEINE = new ItemHolder(37112, 1);
 	private static final ItemHolder ENCHANT_WEAPON_C = new ItemHolder(951, 3);
 	private static final ItemHolder SCROLL_OF_ESCAPE_ALIGATOR_ISLAND = new ItemHolder(37025, 1);
 	private static final ItemHolder STEEL_DOOR_GUILD = new ItemHolder(37045, 21);
-	// Level Condition
+	// Requirements
 	private static final int MIN_LEVEL = 40;
 	private static final int MAX_LEVEL = 45;
-	// Others
-	private static final Location TELE_LOCATION = new Location(-12712, 124902, -3133);
-	private static final String[] COMMAND =
+	// Rewards
+	private static final int EXP_REWARD = 370440;
+	private static final int SP_REWARD = 88;
+	// Teleport from announce stuff
+	// Town masters locations to teleport should be same order as the Race enum
+	private static final Location[] TP_LOCS =
+	{
+		new Location(-13571, 122971, -3107), // human
+		new Location(-13561, 122657, -3105), // elf
+		new Location(-12829, 123163, -3102), // dark elf
+		new Location(-12712, 124902, -3133), // orc
+		new Location(-15236, 124713, -3115), // dwarf
+		new Location(-13520, 125522, -3128), // kamael
+		
+	};
+	private static final String[] TP_COMMANDS =
 	{
 		"Q10390_Teleport"
 	};
 	
 	public Q10390_KekropusLetter()
 	{
-		super(10390, Q10390_KekropusLetter.class.getSimpleName(), "Kekropus Letter");
-		addStartNpc(DRIKUS);
-		addTalkId(DRIKUS, BATHIS, GOSTA, ELI);
+		super(10390, Q10390_KekropusLetter.class.getSimpleName(), "Kekropus' Letter");
+		addStartNpc(DRIKUS, RAYMOND, RAINS, ELLENIA, ESRANDELL, TOBIAS, MENDIO, GERSHWIN);
+		addTalkId(DRIKUS, BATHIS, GOSTA, ELI, RAYMOND, RAINS, ELLENIA, ESRANDELL, TOBIAS, MENDIO, GERSHWIN);
+		registerQuestItems(SCROLL_OF_ESCAPE_ALIGATOR_ISLAND.getId(), SCROLL_OF_ESCAPE_HEINE.getId(), KEKROPUS_LETTER.getId());
 		addCondLevel(MIN_LEVEL, MAX_LEVEL, "30505-noLevel.html");
 		BypassHandler.getInstance().registerHandler(this);
 	}
@@ -85,8 +110,18 @@ public class Q10390_KekropusLetter extends Quest implements IBypassHandler
 		switch (event)
 		{
 			case "30505-03.html":
+			case "30297-03.html":
 			{
 				qs.startQuest();
+				htmltext = event;
+				break;
+			}
+			case "30332-02.html":
+			{
+				if (qs.getQuestItemsCount(KEKROPUS_LETTER.getId()) < 1)
+				{
+					giveItems(player, KEKROPUS_LETTER);
+				}
 				htmltext = event;
 				break;
 			}
@@ -104,6 +139,7 @@ public class Q10390_KekropusLetter extends Quest implements IBypassHandler
 				if (qs.isCond(2))
 				{
 					qs.setCond(3);
+					giveItems(player, SCROLL_OF_ESCAPE_HEINE);
 					htmltext = event;
 				}
 				break;
@@ -124,7 +160,7 @@ public class Q10390_KekropusLetter extends Quest implements IBypassHandler
 				{
 					giveItems(player, ENCHANT_WEAPON_C);
 					giveItems(player, STEEL_DOOR_GUILD);
-					addExpAndSp(player, 370440, 88);
+					addExpAndSp(player, EXP_REWARD, SP_REWARD);
 					showOnScreenMsg(player, NpcStringId.GROW_STRONGER_HERE_UNTIL_YOU_RECEIVE_THE_NEXT_LETTER_FROM_KEKROPUS_AT_LV_46, ExShowScreenMessage.TOP_CENTER, 4500);
 					qs.exitQuest(false, true);
 					htmltext = event;
@@ -132,7 +168,7 @@ public class Q10390_KekropusLetter extends Quest implements IBypassHandler
 				break;
 			}
 			case "30505-02.htm":
-			case "30332-02.html":
+			case "30297-02.htm":
 			case "30916-02.html":
 			{
 				htmltext = event;
@@ -147,74 +183,109 @@ public class Q10390_KekropusLetter extends Quest implements IBypassHandler
 	{
 		final QuestState qs = getQuestState(player, true);
 		String htmltext = getNoQuestMsg(player);
-		if (qs.isCompleted())
-		{
-			return getAlreadyCompletedMsg(player);
-		}
+		
 		if (player.getRace() == Race.ERTHEIA)
 		{
 			return "30505-noErtheia.html";
 		}
 		
-		switch (npc.getId())
+		switch (qs.getState())
 		{
-			case DRIKUS:
+			case State.CREATED:
 			{
-				if (qs.isCreated())
+				switch (npc.getId())
 				{
-					htmltext = "30505-01.htm";
-				}
-				else if (qs.isStarted())
-				{
-					htmltext = "30505-03.html";
-				}
-				break;
-			}
-			case BATHIS:
-			{
-				switch (qs.getCond())
-				{
-					case 1:
+					case RAYMOND:
+					case RAINS:
+					case ELLENIA:
+					case ESRANDELL:
+					case GERSHWIN:
+					case MENDIO:
+					case TOBIAS: // TODO: get all race specified texts
 					{
-						htmltext = "30332-01.html";
+						htmltext = "30297-01.htm";
 						break;
 					}
-					case 2:
+					case DRIKUS:
 					{
-						htmltext = "30332-04.html";
+						if (player.getRace() == Race.ORC)
+						{
+							htmltext = "30505-01.htm";
+						}
+						else
+						{
+							htmltext = getNoQuestMsg(player);
+						}
 						break;
 					}
-					case 3:
+					default:
 					{
-						htmltext = "30332-05.html";
-						break;
-					}
-				}
-				break;
-			}
-			case GOSTA:
-			{
-				switch (qs.getCond())
-				{
-					case 3:
-					{
-						htmltext = "30916-01.html";
-						break;
-					}
-					case 4:
-					{
-						htmltext = "30916-03.html";
+						htmltext = getNoQuestMsg(player);
 						break;
 					}
 				}
 				break;
 			}
-			case ELI:
+			case State.STARTED:
 			{
-				if (qs.isCond(4))
+				switch (npc.getId())
 				{
-					htmltext = "33858-01.html";
+					case RAYMOND:
+					case RAINS:
+					case ELLENIA:
+					case ESRANDELL:
+					case GERSHWIN:
+					case MENDIO:
+					case TOBIAS:// TODO: get all race specified texts
+					{
+						if (qs.isCond(1))
+						{
+							htmltext = "30297-03.html";
+						}
+						break;
+					}
+					case DRIKUS:
+					{
+						if (qs.isCond(1))
+						{
+							htmltext = "30505-03.html";
+						}
+						break;
+					}
+					case BATHIS:
+					{
+						if (qs.isCond(1))
+						{
+							htmltext = "30332-01.html";
+						}
+						else if (qs.isCond(2))
+						{
+							htmltext = "30332-04.html";
+						}
+						break;
+					}
+					case GOSTA:
+					{
+						if (qs.isCond(3))
+						{
+							htmltext = "30916-01.html";
+						}
+						break;
+					}
+					case ELI:
+					{
+						if (qs.isCond(4))
+						{
+							htmltext = "33858-01.html";
+						}
+						break;
+					}
 				}
+				break;
+			}
+			case State.COMPLETED:
+			{
+				htmltext = getAlreadyCompletedMsg(player);
 				break;
 			}
 		}
@@ -235,8 +306,9 @@ public class Q10390_KekropusLetter extends Quest implements IBypassHandler
 			if (qs == null)
 			{
 				final NpcHtmlMessage html = new NpcHtmlMessage(0, 0);
-				html.setHtml(HtmCache.getInstance().getHtm(player.getHtmlPrefix(), "scripts/quests/Q10390_KekropusLetter/Announce.html"));
+				html.setHtml(HtmCache.getInstance().getHtm(player.getHtmlPrefix(), "scripts/quests/Q10390_KekropusLetter/" + "Announce_" + player.getRace().name() + ".html"));
 				player.sendPacket(html);
+				// todo: get proper announce html && handle it
 			}
 			return;
 		}
@@ -246,7 +318,7 @@ public class Q10390_KekropusLetter extends Quest implements IBypassHandler
 	public boolean useBypass(String command, L2PcInstance player, L2Character bypassOrigin)
 	{
 		final QuestState qs = getQuestState(player, false);
-		if ((qs != null) || (player.getLevel() < MIN_LEVEL) || (player.getLevel() > MAX_LEVEL) || (player.getRace() == Race.ERTHEIA))
+		if ((qs != null) || (player.getLevel() < MIN_LEVEL) || (player.getLevel() > MAX_LEVEL) || (player.getRace() == Race.ERTHEIA) || !command.equals("Q10390_Teleport"))
 		{
 			return false;
 		}
@@ -274,7 +346,7 @@ public class Q10390_KekropusLetter extends Quest implements IBypassHandler
 		else
 		{
 			player.getAI().setIntention(CtrlIntention.AI_INTENTION_IDLE);
-			player.teleToLocation(TELE_LOCATION);
+			player.teleToLocation(TP_LOCS[player.getRace().ordinal()]);
 		}
 		return true;
 	}
@@ -282,6 +354,6 @@ public class Q10390_KekropusLetter extends Quest implements IBypassHandler
 	@Override
 	public String[] getBypassList()
 	{
-		return COMMAND;
+		return TP_COMMANDS;
 	}
 }
