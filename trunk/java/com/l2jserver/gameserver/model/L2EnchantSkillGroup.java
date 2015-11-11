7ec9dc9542718b30c7a66433d027f87d1fb67e25
MobiusDev <MobiusDev@7325c9f8-25fd-504a-9f63-8876acdc129b>
2015-11-11 21:44:41 +0000

    Fixed error upon trying to enchant a skill. Contributed by rocknowx.

--- trunk/java/com/l2jserver/gameserver/model/L2EnchantSkillGroup.java
+++ trunk/java/com/l2jserver/gameserver/model/L2EnchantSkillGroup.java
@@ -62,8 +62,8 @@ public final class L2EnchantSkillGroup
 			_adenaCost = set.getInt("adena", 0);
 			_expCost = set.getInt("exp", 0);
 			_spCost = set.getInt("sp", 0);
-			_rate = new byte[24];
-			for (int i = 0; i < 24; i++)
+			_rate = new byte[32];
+			for (int i = 0; i < 32; i++)
 			{
 				_rate[i] = set.getByte("chance" + (76 + i), (byte) 0);
 			}
