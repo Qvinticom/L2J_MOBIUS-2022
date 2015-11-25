1f6da63337325b4b2afae856781ef0c60772ce3c
MobiusDev <MobiusDev@7325c9f8-25fd-504a-9f63-8876acdc129b>
2015-11-25 06:36:05 +0000

    Improvements Q10745_TheSecretIngredients. Contributed by Stayway.

--- trunk/dist/db_installer/sql/game/teleport.sql
+++ trunk/dist/db_installer/sql/game/teleport.sql
@@ -1105,3 +1105,6 @@ INSERT INTO `teleport` VALUES ('DevicetoHellfire', 999001, -147642, 152827, -140
 -- Enchnated Valley
 UPDATE `teleport` SET `Description`='Hunters --> Enchanted Valley',`loc_x`=114681,`loc_y`=48255,`loc_z`=4576 WHERE `Id`=100;
 INSERT INTO `teleport` VALUES ('Herb Collector --> Enchanted Valley ', '3408701', '116703', '53018', '-3632', '0', '0', '57');
+
+-- Whispering Woods
+INSERT INTO `teleport` VALUES ('Dolkin --> Whispering Woods', '3400201', '-88295', '237404', '-2880', '0', '0', '57');
