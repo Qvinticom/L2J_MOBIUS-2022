534e4828d50cb993ce36d52a1ae58bc9459850e9
MobiusDev <MobiusDev@7325c9f8-25fd-504a-9f63-8876acdc129b>
2016-07-17 07:49:05 +0000

    Fixed Pc Cafe request open window.

--- trunk/java/com/l2jmobius/gameserver/network/client/ExIncomingPackets.java
+++ trunk/java/com/l2jmobius/gameserver/network/client/ExIncomingPackets.java
@@ -324,7 +324,7 @@ public enum ExIncomingPackets implements IIncomingPackets<L2GameClient>
 	REQUEST_STOP_MOVE(0xED, null, ConnectionState.IN_GAME),
 	REQUEST_ABILITY_WND_OPEN(0xEE, RequestAbilityWndOpen::new, ConnectionState.IN_GAME),
 	REQUEST_ABILITY_WND_CLOSE(0xEF, RequestAbilityWndClose::new, ConnectionState.IN_GAME),
-	EX_PC_CAFE_REQUEST_OPEN_WINDOW_WITHOUT_NPC(0xF0, null, ConnectionState.IN_GAME),
+	EX_PC_CAFE_REQUEST_OPEN_WINDOW_WITHOUT_NPC(0xF0, ExPCCafeRequestOpenWindowWithoutNPC::new, ConnectionState.IN_GAME),
 	REQUEST_LUCKY_GAME_PLAY(0xF2, null, ConnectionState.IN_GAME),
 	NOTIFY_TRAINING_ROOM_END(0xF3, null, ConnectionState.IN_GAME),
 	REQUEST_NEW_ENCHANT_PUSH_ONE(0xF4, RequestNewEnchantPushOne::new, ConnectionState.IN_GAME),
