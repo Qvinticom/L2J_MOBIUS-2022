2b741e1452e4b9f567bf013d33f5ce485d13a9b7
MobiusDev <MobiusDev@7325c9f8-25fd-504a-9f63-8876acdc129b>
2016-07-29 18:51:18 +0000

    Quest Weakening the Ketra Orc Forces (765). Contributed by gigilo1968.

--- trunk/dist/game/data/scripts/quests/QuestMasterHandler.java
+++ trunk/dist/game/data/scripts/quests/QuestMasterHandler.java
@@ -172,6 +172,7 @@ import quests.Q00761_AssistingTheGoldenRamArmy.Q00761_AssistingTheGoldenRamArmy;
 import quests.Q00762_AnOminousRequest.Q00762_AnOminousRequest;
 import quests.Q00763_ADauntingTask.Q00763_ADauntingTask;
 import quests.Q00764_WeakeningTheVarkaSilenosForces.Q00764_WeakeningTheVarkaSilenosForces;
+import quests.Q00765_WeakeningTheKetraOrcForces.Q00765_WeakeningTheKetraOrcForces;
 import quests.Q00784_TheQuietKiller.Q00784_TheQuietKiller;
 import quests.Q00786_AwaitingTheVoiceOfTheGods.Q00786_AwaitingTheVoiceOfTheGods;
 import quests.Q00901_HowLavasaurusesAreMade.Q00901_HowLavasaurusesAreMade;
@@ -519,6 +520,7 @@ public class QuestMasterHandler
 		Q00762_AnOminousRequest.class,
 		Q00763_ADauntingTask.class,
 		Q00764_WeakeningTheVarkaSilenosForces.class,
+		Q00765_WeakeningTheKetraOrcForces.class,
 		Q00784_TheQuietKiller.class,
 		Q00786_AwaitingTheVoiceOfTheGods.class,
 		Q00901_HowLavasaurusesAreMade.class,
