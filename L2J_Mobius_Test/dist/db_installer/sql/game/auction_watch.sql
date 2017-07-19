100f433c271f2e641c2480338e9dcfbc39f8b36b
MobiusDev <MobiusDev@7325c9f8-25fd-504a-9f63-8876acdc129b>
2017-07-19 20:35:12 +0000

    Removed Test project.

--- L2J_Mobius_Test/dist/db_installer/sql/game/auction_watch.sql
+++
@@ -1,6 +0,0 @@
-DROP TABLE IF EXISTS `auction_watch`;
-CREATE TABLE IF NOT EXISTS `auction_watch` (
-  `charObjId` INT NOT NULL DEFAULT 0,
-  `auctionId` INT NOT NULL DEFAULT 0,
-  PRIMARY KEY (`charObjId`,`auctionId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
--- L2J_Mobius_Test/dist/db_installer/sql/game/item_soulcrystal.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS `item_soulcrystal`;
-CREATE TABLE `item_soulcrystal` (
-  `object_id` int(11) NOT NULL,
-  `slot_id` int(1) NOT NULL,
-  `is_special` bit(1) NOT NULL,
-  `effect_id` int(4) NOT NULL,
-  PRIMARY KEY (`object_id`,`slot_id`,`is_special`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
