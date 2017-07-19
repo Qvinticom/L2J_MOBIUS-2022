70e9242fdc931ede424d6d69f233f7ce1cd275fb
MobiusDev <MobiusDev@7325c9f8-25fd-504a-9f63-8876acdc129b>
2017-07-19 21:51:33 +0000

    Moved bosses ai to separate package.

--- L2J_Mobius_C4/dist/game/data/scripts/ai/bosses/Antharas.java
+++ L2J_Mobius_C4/dist/game/data/scripts/ai/bosses/Antharas.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-package ai.individual;
+package ai.bosses;
 
 import com.l2jmobius.gameserver.ai.CtrlIntention;
 import com.l2jmobius.gameserver.instancemanager.GrandBossManager;
--- L2J_Mobius_C4/dist/game/data/scripts/ai/bosses/Valakas.java
+++ L2J_Mobius_C4/dist/game/data/scripts/ai/bosses/Valakas.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-package ai.individual;
+package ai.bosses;
 
 import static com.l2jmobius.gameserver.ai.CtrlIntention.AI_INTENTION_CAST;
 import static com.l2jmobius.gameserver.ai.CtrlIntention.AI_INTENTION_FOLLOW;
