a0974c8d9d7348b028ed4bc7fbe9bfc99105a45c
MobiusDev <MobiusDev@7325c9f8-25fd-504a-9f63-8876acdc129b>
2017-08-08 12:19:36 +0000

    Complete set of classes for cron4j.

--- L2J_Mobius_Classic/java/com/l2jmobius/gameserver/util/cron4j/ValueMatcher.java
+++ L2J_Mobius_Classic/java/com/l2jmobius/gameserver/util/cron4j/ValueMatcher.java
@@ -1,18 +1,20 @@
 /*
- * This file is part of the L2J Mobius project.
+ * cron4j - A pure Java cron-like scheduler
+ * 
+ * Copyright (C) 2007-2010 Carlo Pelliccia (www.sauronsoftware.it)
  * 
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
+ * it under the terms of the GNU Lesser General Public License version
+ * 2.1, as published by the Free Software Foundation.
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License 2.1 for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License version 2.1 along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
  */
 package com.l2jmobius.gameserver.util.cron4j;
 
