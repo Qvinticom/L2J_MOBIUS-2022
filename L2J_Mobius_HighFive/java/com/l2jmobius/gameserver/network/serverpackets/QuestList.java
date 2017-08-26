66203fa13f9fe28a01c9da485cde5000f68e0ca6
MobiusDev <MobiusDev@7325c9f8-25fd-504a-9f63-8876acdc129b>
2017-08-26 01:07:32 +0000

    Solve wrong quest state show in the window.

--- L2J_Mobius_HighFive/java/com/l2jmobius/gameserver/network/serverpackets/QuestList.java
+++ L2J_Mobius_HighFive/java/com/l2jmobius/gameserver/network/serverpackets/QuestList.java
@@ -81,7 +81,7 @@ public class QuestList extends L2GameServerPacket
 			}
 			
 			int states = qs.getInt("__compltdStateFlags");
-			if (states != 0)
+			if (states > 0)
 			{
 				writeD(states);
 			}
