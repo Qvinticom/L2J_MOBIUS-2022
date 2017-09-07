f3648a2950503742d51a1d428d328c0991d41cb0
MobiusDev <MobiusDev@7325c9f8-25fd-504a-9f63-8876acdc129b>
2017-09-07 02:58:22 +0000

    Fixed siege door targeting with enabled geodata.

--- L2J_Mobius_3.0_Helios/java/com/l2jmobius/gameserver/model/actor/L2Character.java
+++ L2J_Mobius_3.0_Helios/java/com/l2jmobius/gameserver/model/actor/L2Character.java
@@ -1051,15 +1051,13 @@ public abstract class L2Character extends L2Object implements ISkillsHolder, IDe
 			// GeoData Los Check here (or dz > 1000)
 			if (!GeoEngine.getInstance().canSeeTarget(this, target))
 			{
-				if (target.isDoor() && target.isAutoAttackable(this))
+				if (!target.isDoor() || !target.isAutoAttackable(this))
 				{
-					getAI().setIntention(CtrlIntention.AI_INTENTION_MOVE_TO, target.getLocation());
+					sendPacket(SystemMessageId.CANNOT_SEE_TARGET);
+					getAI().setIntention(CtrlIntention.AI_INTENTION_ACTIVE);
+					sendPacket(ActionFailed.STATIC_PACKET);
 					return;
 				}
-				sendPacket(SystemMessageId.CANNOT_SEE_TARGET);
-				getAI().setIntention(CtrlIntention.AI_INTENTION_ACTIVE);
-				sendPacket(ActionFailed.STATIC_PACKET);
-				return;
 			}
 			
 			// BOW and CROSSBOW checks
