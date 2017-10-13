cda9458610ff3992ff3cec0050cb36159dbd9b5f
MobiusDev <MobiusDev@7325c9f8-25fd-504a-9f63-8876acdc129b>
2017-10-13 12:58:44 +0000

    Summon levels should be affected by player max level.

--- L2J_Mobius_2.5_Underground/dist/game/data/scripts/handlers/effecthandlers/SummonMulti.java
+++ L2J_Mobius_2.5_Underground/dist/game/data/scripts/handlers/effecthandlers/SummonMulti.java
@@ -83,14 +83,14 @@ public final class SummonMulti extends AbstractEffect
 		summon.setItemConsume(_consumeItem);
 		summon.setItemConsumeInterval(consumeItemInterval);
 		
-		if (summon.getLevel() >= ExperienceData.getInstance().getMaxPetLevel())
+		if (summon.getLevel() >= ExperienceData.getInstance().getMaxLevel())
 		{
-			summon.getStat().setExp(ExperienceData.getInstance().getExpForLevel(ExperienceData.getInstance().getMaxPetLevel() - 1));
-			_log.warning(getClass().getSimpleName() + ": (" + summon.getName() + ") NpcID: " + summon.getId() + " has a level above " + ExperienceData.getInstance().getMaxPetLevel() + ". Please rectify.");
+			summon.getStat().setExp(ExperienceData.getInstance().getExpForLevel(ExperienceData.getInstance().getMaxLevel() - 1));
+			_log.warning(getClass().getSimpleName() + ": (" + summon.getName() + ") NpcID: " + summon.getId() + " has a level above " + ExperienceData.getInstance().getMaxLevel() + ". Please rectify.");
 		}
 		else
 		{
-			summon.getStat().setExp(ExperienceData.getInstance().getExpForLevel(summon.getLevel() % ExperienceData.getInstance().getMaxPetLevel()));
+			summon.getStat().setExp(ExperienceData.getInstance().getExpForLevel(summon.getLevel() % ExperienceData.getInstance().getMaxLevel()));
 		}
 		
 		summon.setCurrentHp(summon.getMaxHp());
