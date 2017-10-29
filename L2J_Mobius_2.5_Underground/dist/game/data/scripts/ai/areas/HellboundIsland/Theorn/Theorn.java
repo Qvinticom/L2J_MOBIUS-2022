6a18303729b4aaf6fefa630eddaf19dc81e4e65e
MobiusDev <MobiusDev@7325c9f8-25fd-504a-9f63-8876acdc129b>
2017-10-29 23:21:50 +0000

    Proper name for Theorn package.

--- L2J_Mobius_2.5_Underground/dist/game/data/scripts/ai/areas/HellboundIsland/Theorn/Theorn.java
+++ L2J_Mobius_2.5_Underground/dist/game/data/scripts/ai/areas/HellboundIsland/Theorn/Theorn.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-package ai.areas.HellboundIsland.Theron;
+package ai.areas.HellboundIsland.Theorn;
 
 import com.l2jmobius.gameserver.data.xml.impl.SkillData;
 import com.l2jmobius.gameserver.enums.ChatType;
