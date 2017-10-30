58d9e1ad338e70aef1f39e75896d6b4bb656fc6e
MobiusDev <MobiusDev@7325c9f8-25fd-504a-9f63-8876acdc129b>
2017-10-30 22:22:56 +0000

    Addition of ResetInstanceEntry effect handler.

--- L2J_Mobius_1.0_Ertheia/dist/game/data/scripts/handlers/EffectMasterHandler.java
+++ L2J_Mobius_1.0_Ertheia/dist/game/data/scripts/handlers/EffectMasterHandler.java
@@ -262,6 +262,7 @@ public final class EffectMasterHandler
 		EffectHandler.getInstance().registerHandler("ReflectSkill", ReflectSkill::new);
 		EffectHandler.getInstance().registerHandler("RefuelAirship", RefuelAirship::new);
 		EffectHandler.getInstance().registerHandler("Relax", Relax::new);
+		EffectHandler.getInstance().registerHandler("ResetInstanceEntry", ResetInstanceEntry::new);
 		EffectHandler.getInstance().registerHandler("ResistAbnormalByCategory", ResistAbnormalByCategory::new);
 		EffectHandler.getInstance().registerHandler("ResistDDMagic", ResistDDMagic::new);
 		EffectHandler.getInstance().registerHandler("ResistDispelByCategory", ResistDispelByCategory::new);
