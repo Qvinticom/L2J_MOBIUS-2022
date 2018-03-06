cef85659596807adbed280d29cf959aed5de71e8
MobiusDev <MobiusDev@7325c9f8-25fd-504a-9f63-8876acdc129b>
2018-03-06 08:38:42 +0000

    Moved SummonPc AI to areas CrumaTower folder.

--- L2J_Mobius_Classic_2.0_Saviors/dist/game/data/scripts/ai/areas/CrumaTower/SummonPc.java
+++ L2J_Mobius_Classic_2.0_Saviors/dist/game/data/scripts/ai/areas/CrumaTower/SummonPc.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-package ai.others;
+package ai.areas.CrumaTower;
 
 import com.l2jmobius.gameserver.model.actor.L2Attackable;
 import com.l2jmobius.gameserver.model.actor.L2Npc;
