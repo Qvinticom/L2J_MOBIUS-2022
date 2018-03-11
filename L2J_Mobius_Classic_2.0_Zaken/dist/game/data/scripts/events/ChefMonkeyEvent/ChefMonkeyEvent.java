4bbd1913d4f7c1c0e43ec9ccf09001d64530daf1
MobiusDev <MobiusDev@7325c9f8-25fd-504a-9f63-8876acdc129b>
2018-03-11 14:01:25 +0000

    Proper Chef Monkey event.

--- L2J_Mobius_Classic_2.0_Zaken/dist/game/data/scripts/events/ChefMonkeyEvent/ChefMonkeyEvent.java
+++ L2J_Mobius_Classic_2.0_Zaken/dist/game/data/scripts/events/ChefMonkeyEvent/ChefMonkeyEvent.java
@@ -16,89 +16,31 @@
  */
 package events.ChefMonkeyEvent;
 
-import com.l2jmobius.gameserver.data.xml.impl.MultisellData;
 import com.l2jmobius.gameserver.model.actor.L2Npc;
 import com.l2jmobius.gameserver.model.actor.instance.L2PcInstance;
-import com.l2jmobius.gameserver.model.events.EventType;
-import com.l2jmobius.gameserver.model.events.ListenerRegisterType;
-import com.l2jmobius.gameserver.model.events.annotations.Id;
-import com.l2jmobius.gameserver.model.events.annotations.RegisterEvent;
-import com.l2jmobius.gameserver.model.events.annotations.RegisterType;
-import com.l2jmobius.gameserver.model.events.impl.character.npc.OnNpcMenuSelect;
 import com.l2jmobius.gameserver.model.quest.LongTimeEvent;
 
 /**
- * Chef Monkey Event<br>
- * http://www.lineage2.com/en/news/events/chef-monkey-event-2016.php
- * @author ChaosPaladin
+ * Chef Monkey Event
+ * @URL https://eu.4gameforum.com/threads/603119/
+ * @author Mobius
  */
 public final class ChefMonkeyEvent extends LongTimeEvent
 {
-	// NPCs
-	private static final int EV_CHEF_MONKEY = 34292;
+	// NPC
+	private static final int CHEF_MONKEY = 34292;
 	
 	private ChefMonkeyEvent()
 	{
-		addStartNpc(EV_CHEF_MONKEY);
-		addFirstTalkId(EV_CHEF_MONKEY);
-		addTalkId(EV_CHEF_MONKEY);
+		addStartNpc(CHEF_MONKEY);
+		addFirstTalkId(CHEF_MONKEY);
+		addTalkId(CHEF_MONKEY);
 	}
 	
 	@Override
 	public String onFirstTalk(L2Npc npc, L2PcInstance player)
 	{
-		return "ev_chef_monkey001.htm";
-	}
-	
-	@Override
-	public String onAdvEvent(String event, L2Npc npc, L2PcInstance player)
-	{
-		String htmltext = null;
-		
-		switch (event)
-		{
-			case "ev_chef_monkey001.htm":
-			case "ev_chef_monkey002.htm":
-			case "ev_chef_monkey003.htm":
-			case "ev_chef_monkey004.htm":
-			case "ev_chef_monkey005.htm":
-			{
-				htmltext = event;
-				break;
-			}
-		}
-		return htmltext;
-	}
-	
-	@RegisterEvent(EventType.ON_NPC_MENU_SELECT)
-	@RegisterType(ListenerRegisterType.NPC)
-	@Id(EV_CHEF_MONKEY)
-	public final void OnNpcMenuSelect(OnNpcMenuSelect event)
-	{
-		final L2PcInstance player = event.getTalker();
-		final L2Npc npc = event.getNpc();
-		final int ask = event.getAsk();
-		final int reply = event.getReply();
-		
-		if (ask == -303)
-		{
-			if (reply == 2209)
-			{
-				// I want to buy Monkey Bait
-				MultisellData.getInstance().separateAndSend(2209, player, npc, false);
-			}
-			else if (reply == 2212)
-			{
-				// I want to exchange Tuna fish
-				MultisellData.getInstance().separateAndSend(2212, player, npc, false);
-			}
-			else if (reply == 3043)
-			{
-				// buy buff cocktail
-				MultisellData.getInstance().separateAndSend(3043, player, npc, false);
-				
-			}
-		}
+		return "34292-01.htm";
 	}
 	
 	public static void main(String[] args)
