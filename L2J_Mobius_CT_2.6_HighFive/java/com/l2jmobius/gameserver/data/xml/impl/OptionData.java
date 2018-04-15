9b3553a51fe9b2fe63d4dbb8812a03f9f1750fc6
MobiusDev <MobiusDev@7325c9f8-25fd-504a-9f63-8876acdc129b>
2018-04-15 16:38:24 +0000

    Moved options in augmentation folder.

--- L2J_Mobius_CT_2.6_HighFive/java/com/l2jmobius/gameserver/data/xml/impl/OptionData.java
+++ L2J_Mobius_CT_2.6_HighFive/java/com/l2jmobius/gameserver/data/xml/impl/OptionData.java
@@ -50,7 +50,7 @@ public class OptionData implements IGameXmlReader
 	public synchronized void load()
 	{
 		_optionData.clear();
-		parseDatapackDirectory("data/stats/options", false);
+		parseDatapackDirectory("data/stats/augmentation/options", false);
 		LOGGER.log(Level.INFO, getClass().getSimpleName() + ": Loaded: " + _optionData.size() + " Options.");
 	}
 	
