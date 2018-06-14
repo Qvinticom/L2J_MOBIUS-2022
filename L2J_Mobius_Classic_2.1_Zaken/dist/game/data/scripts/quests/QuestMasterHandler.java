3f66fc29e31fa805774fac8a4f2436878df76a94
MobiusDev <MobiusDev@7325c9f8-25fd-504a-9f63-8876acdc129b>
2018-06-14 21:15:45 +0000

    Addition of quest Future: Elves (11012). Contributed by Stayway.

--- L2J_Mobius_Classic_2.1_Zaken/dist/game/data/scripts/quests/QuestMasterHandler.java
+++ L2J_Mobius_Classic_2.1_Zaken/dist/game/data/scripts/quests/QuestMasterHandler.java
@@ -166,6 +166,7 @@ import quests.Q11008_PreparationForDungeon.Q11008_PreparationForDungeon;
 import quests.Q11009_NewPotionDevelopment1.Q11009_NewPotionDevelopment1;
 import quests.Q11010_NewPotionDevelopment2.Q11010_NewPotionDevelopment2;
 import quests.Q11011_NewPotionDevelopment3.Q11011_NewPotionDevelopment3;
+import quests.Q11012_FutureElves.Q11012_FutureElves;
 import quests.Q11013_ShilensHunt.Q11013_ShilensHunt;
 import quests.Q11014_SurpriseGift.Q11014_SurpriseGift;
 import quests.Q11015_PrepareForTrade1.Q11015_PrepareForTrade1;
@@ -334,6 +335,7 @@ public class QuestMasterHandler
 		Q11009_NewPotionDevelopment1.class,
 		Q11010_NewPotionDevelopment2.class,
 		Q11011_NewPotionDevelopment3.class,
+		Q11012_FutureElves.class,
 		Q11013_ShilensHunt.class,
 		Q11014_SurpriseGift.class,
 		Q11015_PrepareForTrade1.class,
