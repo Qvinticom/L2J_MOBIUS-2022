f1ac915b128791af0ed5ebd7e8f2a58822fe4701
MobiusDev <MobiusDev@7325c9f8-25fd-504a-9f63-8876acdc129b>
2018-09-06 12:04:08 +0000

    You should be able to start Clan Wars at clan level 3.

--- L2J_Mobius_Classic_2.2_Antharas/java/com/l2jmobius/gameserver/network/clientpackets/RequestStartPledgeWar.java
+++ L2J_Mobius_Classic_2.2_Antharas/java/com/l2jmobius/gameserver/network/clientpackets/RequestStartPledgeWar.java
@@ -59,7 +59,7 @@ public final class RequestStartPledgeWar implements IClientIncomingPacket
 			return;
 		}
 		
-		if ((clanDeclaringWar.getLevel() < 5) || (clanDeclaringWar.getMembersCount() < Config.ALT_CLAN_MEMBERS_FOR_WAR))
+		if ((clanDeclaringWar.getLevel() < 3) || (clanDeclaringWar.getMembersCount() < Config.ALT_CLAN_MEMBERS_FOR_WAR))
 		{
 			client.sendPacket(SystemMessage.getSystemMessage(SystemMessageId.A_CLAN_WAR_CAN_ONLY_BE_DECLARED_IF_THE_CLAN_IS_LEVEL_3_OR_ABOVE_AND_THE_NUMBER_OF_CLAN_MEMBERS_IS_15_OR_GREATER));
 			client.sendPacket(ActionFailed.STATIC_PACKET);
@@ -97,7 +97,7 @@ public final class RequestStartPledgeWar implements IClientIncomingPacket
 			client.sendPacket(ActionFailed.STATIC_PACKET);
 			return;
 		}
-		else if ((clanDeclaredWar.getLevel() < 5) || (clanDeclaredWar.getMembersCount() < Config.ALT_CLAN_MEMBERS_FOR_WAR))
+		else if ((clanDeclaredWar.getLevel() < 3) || (clanDeclaredWar.getMembersCount() < Config.ALT_CLAN_MEMBERS_FOR_WAR))
 		{
 			client.sendPacket(SystemMessage.getSystemMessage(SystemMessageId.A_CLAN_WAR_CAN_ONLY_BE_DECLARED_IF_THE_CLAN_IS_LEVEL_3_OR_ABOVE_AND_THE_NUMBER_OF_CLAN_MEMBERS_IS_15_OR_GREATER));
 			client.sendPacket(ActionFailed.STATIC_PACKET);
