a8b899441371fa63fd43e2d32731150786f0dc86
MobiusDev <MobiusDev@7325c9f8-25fd-504a-9f63-8876acdc129b>
2019-03-03 11:19:57 +0000

    Removed quest For the Forgotten Heroes (10304).

--- L2J_Mobius_6.0_Fafurion/dist/game/data/scripts/quests/QuestMasterHandler.java
+++ L2J_Mobius_6.0_Fafurion/dist/game/data/scripts/quests/QuestMasterHandler.java
@@ -182,7 +182,6 @@ import quests.Q10297_GrandOpeningComeToOurPub.Q10297_GrandOpeningComeToOurPub;
 import quests.Q10301_ShadowOfTerrorBlackishRedFog.Q10301_ShadowOfTerrorBlackishRedFog;
 import quests.Q10302_UnsettlingShadowAndRumors.Q10302_UnsettlingShadowAndRumors;
 import quests.Q10303_CrossroadsBetweenLightAndDarkness.Q10303_CrossroadsBetweenLightAndDarkness;
-import quests.Q10304_ForForgottenHeroes.Q10304_ForForgottenHeroes;
 import quests.Q10305_UnstoppableFutileEfforts.Q10305_UnstoppableFutileEfforts;
 import quests.Q10306_TheCorruptedLeader.Q10306_TheCorruptedLeader;
 import quests.Q10307_TheCorruptedLeaderHisTruth.Q10307_TheCorruptedLeaderHisTruth;
@@ -563,7 +562,6 @@ public class QuestMasterHandler
 		Q10301_ShadowOfTerrorBlackishRedFog.class,
 		Q10302_UnsettlingShadowAndRumors.class,
 		Q10303_CrossroadsBetweenLightAndDarkness.class,
-		Q10304_ForForgottenHeroes.class,
 		Q10305_UnstoppableFutileEfforts.class,
 		Q10306_TheCorruptedLeader.class,
 		Q10307_TheCorruptedLeaderHisTruth.class,
