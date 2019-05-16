4f08e2f56ad9b2ac644eb7adb1cf906445b0b5a2
MobiusDevelopment <MobiusDevelopment@7325c9f8-25fd-504a-9f63-8876acdc129b>
2019-05-16 00:02:04 +0000

    Proper CanUseInBattlefield skill condition.

--- L2J_Mobius_3.0_Helios/dist/game/data/scripts/handlers/skillconditionhandlers/CanUseInBattlefieldSkillCondition.java
+++ L2J_Mobius_3.0_Helios/dist/game/data/scripts/handlers/skillconditionhandlers/CanUseInBattlefieldSkillCondition.java
@@ -21,17 +21,20 @@ import org.l2jmobius.gameserver.model.WorldObject;
 import org.l2jmobius.gameserver.model.actor.Creature;
 import org.l2jmobius.gameserver.model.skills.ISkillCondition;
 import org.l2jmobius.gameserver.model.skills.Skill;
+import org.l2jmobius.gameserver.model.zone.ZoneId;
 
+/**
+ * @author Mobius
+ */
 public class CanUseInBattlefieldSkillCondition implements ISkillCondition
 {
 	public CanUseInBattlefieldSkillCondition(StatsSet params)
 	{
-		
 	}
 	
 	@Override
 	public boolean canUse(Creature caster, Skill skill, WorldObject target)
 	{
-		return true; // TODO
+		return (caster != null) && caster.isInsideZone(ZoneId.SIEGE);
 	}
 }
