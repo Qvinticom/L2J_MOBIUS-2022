99d341771c257a2cd01aee6b68f75d1ebb6bf0e4
MobiusDevelopment <MobiusDevelopment@7325c9f8-25fd-504a-9f63-8876acdc129b>
2019-10-23 08:53:56 +0000

    Fixed Hardin ClassId ordinal NPE.

--- L2J_Mobius_2.5_Underground/dist/game/data/scripts/ai/areas/TalkingIsland/Hardin/Hardin.java
+++ L2J_Mobius_2.5_Underground/dist/game/data/scripts/ai/areas/TalkingIsland/Hardin/Hardin.java
@@ -112,7 +112,7 @@ public class Hardin extends AbstractNpcAI
 					}
 				}
 				classes.append("<button value=\"");
-				classes.append(ClassListData.getInstance().getClass(c.ordinal()).getClassName());
+				classes.append(ClassListData.getInstance().getClass(c.getId()).getClassName());
 				classes.append("\" action=\"bypass -h Quest Hardin try_");
 				classes.append(String.valueOf(c.getId()));
 				classes.append("\" width=\"200\" height=\"31\" back=\"L2UI_CT1.HtmlWnd_DF_Awake_Down\" fore=\"L2UI_CT1.HtmlWnd_DF_Awake\"><br1>");
