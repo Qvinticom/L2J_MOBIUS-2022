9cfe235f652fc4d02fc03871c8d8d223e39db108
MobiusDevelopment <MobiusDevelopment@7325c9f8-25fd-504a-9f63-8876acdc129b>
2019-11-25 18:58:50 +0000

    Addition of Joachim AI. Contributed by NightBR.

---
+++ L2J_Mobius_7.0_PreludeOfWar/dist/game/data/scripts/ai/areas/Aden/Joachim/Joachim.java
@@ -0,0 +1,69 @@
+/*
+ * This file is part of the L2J Mobius project.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package ai.areas.Aden.Joachim;
+
+import org.l2jmobius.gameserver.model.actor.Npc;
+import org.l2jmobius.gameserver.model.actor.instance.PlayerInstance;
+
+import ai.AbstractNpcAI;
+
+/**
+ * Aden Faction Npc AI
+ * @author NightBR
+ * @date 2019-11-25
+ */
+public class Joachim extends AbstractNpcAI
+{
+	// NPC
+	private static final int Joachim = 34513;
+	
+	private Joachim()
+	{
+		addStartNpc(Joachim);
+		addTalkId(Joachim);
+		addFirstTalkId(Joachim);
+	}
+	
+	@Override
+	public String onAdvEvent(String event, Npc npc, PlayerInstance player)
+	{
+		switch (event)
+		{
+			case "34513-01.html":
+			case "34513-02.html":
+			case "34513-03.html":
+			{
+				return event;
+			}
+			default:
+			{
+				return null;
+			}
+		}
+	}
+	
+	@Override
+	public String onFirstTalk(Npc npc, PlayerInstance player)
+	{
+		return "34513.html";
+	}
+	
+	public static void main(String[] args)
+	{
+		new Joachim();
+	}
+}
