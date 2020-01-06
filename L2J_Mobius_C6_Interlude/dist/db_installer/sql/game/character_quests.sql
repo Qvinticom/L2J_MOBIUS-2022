f1af520495aa9e7d187219be3a862644df5b07e9
MobiusDevelopment <MobiusDevelopment@7325c9f8-25fd-504a-9f63-8876acdc129b>
2020-01-06 14:06:19 +0000

    Removed class_index field from character_quests table. Contributed by ReynalDev.

--- L2J_Mobius_C6_Interlude/dist/db_installer/sql/game/character_quests.sql
+++ L2J_Mobius_C6_Interlude/dist/db_installer/sql/game/character_quests.sql
@@ -6,6 +6,5 @@ CREATE TABLE IF NOT EXISTS `character_quests` (
   `name` VARCHAR(40) NOT NULL DEFAULT '',
   `var`  VARCHAR(20) NOT NULL DEFAULT '',
   `value` VARCHAR(255) ,
-  `class_index` int(1) NOT NULL default '0',
   PRIMARY KEY  (`char_id`,`name`,`var`,`class_index`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
--- L2J_Mobius_CT_2.4_Epilogue/dist/db_installer/sql/game/character_quests.sql
+++ L2J_Mobius_CT_2.4_Epilogue/dist/db_installer/sql/game/character_quests.sql
@@ -3,6 +3,5 @@ CREATE TABLE IF NOT EXISTS `character_quests` (
   `name` VARCHAR(60) NOT NULL DEFAULT '',
   `var`  VARCHAR(20) NOT NULL DEFAULT '',
   `value` VARCHAR(255) ,
-  `class_index` int(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`charId`,`name`,`var`,`class_index`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
