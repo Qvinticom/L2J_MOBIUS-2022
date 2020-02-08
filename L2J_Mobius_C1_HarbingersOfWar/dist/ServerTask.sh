2b85b6c52df93d6004468f239143a70ef2231bac
MobiusDevelopment <MobiusDevelopment@7325c9f8-25fd-504a-9f63-8876acdc129b>
2020-02-08 11:37:19 +0000

    Force headless parameter with Linux sh files. Contributed by MutatedSkull.

--- L2J_Mobius_C1_HarbingersOfWar/dist/ServerTask.sh
+++ L2J_Mobius_C1_HarbingersOfWar/dist/ServerTask.sh
@@ -7,7 +7,7 @@
 while :; do
 	[ -f log/java0.log.0 ] && mv log/java0.log.0 "log/`date +%Y-%m-%d_%H-%M-%S`_java.log"
 	[ -f log/stdout.log ] && mv log/stdout.log "log/`date +%Y-%m-%d_%H-%M-%S`_stdout.log"
-	java $(cat "java.cfg") -jar Server.jar > log/stdout.log 2>&1
+	java -Djava.awt.headless=true $(cat "java.cfg") -jar Server.jar > log/stdout.log 2>&1
 	[ $? -ne 2 ] && break
 #	/etc/init.d/mysql restart
 	sleep 10
