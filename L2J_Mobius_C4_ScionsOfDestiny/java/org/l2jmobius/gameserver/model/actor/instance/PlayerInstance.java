f3e77865e6e7f82be0228c0e8a826db07aeae11c
MobiusDevelopment <MobiusDevelopment@7325c9f8-25fd-504a-9f63-8876acdc129b>
2020-08-28 11:32:59 +0000

    Minor message improvement related with previous commits.

--- L2J_Mobius_C4_ScionsOfDestiny/java/org/l2jmobius/gameserver/model/actor/instance/PlayerInstance.java
+++ L2J_Mobius_C4_ScionsOfDestiny/java/org/l2jmobius/gameserver/model/actor/instance/PlayerInstance.java
@@ -664,7 +664,7 @@ public class PlayerInstance extends Playable
 				final Siege siege = SiegeManager.getInstance().getSiege(PlayerInstance.this);
 				if ((((siege == null) || !siege.isInProgress()) && ((thisLevel >= 20) && (targetLevel < 20))) || ((thisLevel >= 40) && (targetLevel < 40)) || ((thisLevel >= 52) && (targetLevel < 52)) || ((thisLevel >= 61) && (targetLevel < 61)) || ((thisLevel >= 76) && (targetLevel < 76)))
 				{
-					sendMessage("You can only engage in PvP if your target is within your level grade.");
+					sendMessage("You can only engage in PvP when your target is within your level grade.");
 					sendPacket(ActionFailed.STATIC_PACKET);
 					return;
 				}
@@ -9864,7 +9864,7 @@ public class PlayerInstance extends Playable
 			final Siege siege = SiegeManager.getInstance().getSiege(this);
 			if ((((siege == null) || !siege.isInProgress()) && ((thisLevel >= 20) && (targetLevel < 20))) || ((thisLevel >= 40) && (targetLevel < 40)) || ((thisLevel >= 52) && (targetLevel < 52)) || ((thisLevel >= 61) && (targetLevel < 61)) || ((thisLevel >= 76) && (targetLevel < 76)))
 			{
-				sendMessage("You can only engage in PvP if your target is within your level grade.");
+				sendMessage("You can only engage in PvP when your target is within your level grade.");
 				sendPacket(ActionFailed.STATIC_PACKET);
 				return;
 			}
