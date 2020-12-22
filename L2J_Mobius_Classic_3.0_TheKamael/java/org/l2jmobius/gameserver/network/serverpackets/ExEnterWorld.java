993906ed2bc611c1302cd860bb2e081768731666
MobiusDevelopment <MobiusDevelopment@7325c9f8-25fd-504a-9f63-8876acdc129b>
2020-12-22 12:01:57 +0000

    ExEnterWorld improvements. Contributed by Mode.

--- L2J_Mobius_Classic_3.0_TheKamael/java/org/l2jmobius/gameserver/network/serverpackets/ExEnterWorld.java
+++ L2J_Mobius_Classic_3.0_TheKamael/java/org/l2jmobius/gameserver/network/serverpackets/ExEnterWorld.java
@@ -16,6 +16,11 @@
  */
 package org.l2jmobius.gameserver.network.serverpackets;
 
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.zone.ZoneRules;
+
+import org.l2jmobius.Config;
 import org.l2jmobius.commons.network.PacketWriter;
 import org.l2jmobius.gameserver.network.OutgoingPackets;
 
@@ -24,18 +29,27 @@ import org.l2jmobius.gameserver.network.OutgoingPackets;
  */
 public class ExEnterWorld implements IClientOutgoingPacket
 {
+	private final int _zoneIdOffsetSeconds;
+	private final int _epochInSeconds;
+	private final int _daylight;
+	
 	public ExEnterWorld()
 	{
+		Instant now = Instant.now();
+		_epochInSeconds = (int) now.getEpochSecond();
+		ZoneRules rules = ZoneId.systemDefault().getRules();
+		_zoneIdOffsetSeconds = rules.getStandardOffset(now).getTotalSeconds();
+		_daylight = (int) rules.getDaylightSavings(now).toSeconds();
 	}
 	
 	@Override
 	public boolean write(PacketWriter packet)
 	{
 		OutgoingPackets.EX_ENTER_WORLD.writeId(packet);
-		packet.writeD((int) (System.currentTimeMillis() / 1000));
-		packet.writeD(0x00);
-		packet.writeD(0x00);
-		packet.writeD(0x00);
+		packet.writeD(_epochInSeconds);
+		packet.writeD(-_zoneIdOffsetSeconds);
+		packet.writeD(_daylight);
+		packet.writeD(Config.SERVER_ID);
 		return true;
 	}
 }
