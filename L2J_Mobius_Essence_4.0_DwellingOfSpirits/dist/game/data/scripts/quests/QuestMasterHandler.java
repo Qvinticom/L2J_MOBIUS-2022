7c7ece4330c08da4b5d7bb20e99728891b899e74
MobiusDevelopment <MobiusDevelopment@7325c9f8-25fd-504a-9f63-8876acdc129b>
2021-01-20 01:28:36 +0000

    Quest cleanup.

--- L2J_Mobius_Essence_4.0_DwellingOfSpirits/dist/game/data/scripts/quests/QuestMasterHandler.java
+++ L2J_Mobius_Essence_4.0_DwellingOfSpirits/dist/game/data/scripts/quests/QuestMasterHandler.java
@@ -21,33 +21,14 @@ import java.util.logging.Logger;
 
 import quests.Q00127_FishingSpecialistsRequest.Q00127_FishingSpecialistsRequest;
 import quests.Q00255_Tutorial.Q00255_Tutorial;
-import quests.Q00300_HuntingLetoLizardman.Q00300_HuntingLetoLizardman;
-import quests.Q00326_VanquishRemnants.Q00326_VanquishRemnants;
-import quests.Q00327_RecoverTheFarmland.Q00327_RecoverTheFarmland;
-import quests.Q00328_SenseForBusiness.Q00328_SenseForBusiness;
-import quests.Q00329_CuriosityOfADwarf.Q00329_CuriosityOfADwarf;
-import quests.Q00331_ArrowOfVengeance.Q00331_ArrowOfVengeance;
-import quests.Q00333_HuntOfTheBlackLion.Q00333_HuntOfTheBlackLion;
-import quests.Q00344_1000YearsTheEndOfLamentation.Q00344_1000YearsTheEndOfLamentation;
-import quests.Q00354_ConquestOfAlligatorIsland.Q00354_ConquestOfAlligatorIsland;
-import quests.Q00355_FamilyHonor.Q00355_FamilyHonor;
-import quests.Q00356_DigUpTheSeaOfSpores.Q00356_DigUpTheSeaOfSpores;
-import quests.Q00358_IllegitimateChildOfTheGoddess.Q00358_IllegitimateChildOfTheGoddess;
-import quests.Q00360_PlunderTheirSupplies.Q00360_PlunderTheirSupplies;
-import quests.Q00369_CollectorOfJewels.Q00369_CollectorOfJewels;
-import quests.Q00370_AnElderSowsSeeds.Q00370_AnElderSowsSeeds;
-import quests.Q00500_BrothersBoundInChains.Q00500_BrothersBoundInChains;
+import quests.Q00502_BrothersBoundInChains.Q00502_BrothersBoundInChains;
 import quests.Q00662_AGameOfCards.Q00662_AGameOfCards;
-import quests.Q00933_ExploringTheWestWingOfTheDungeonOfAbyss.Q00933_ExploringTheWestWingOfTheDungeonOfAbyss;
-import quests.Q00935_ExploringTheEastWingOfTheDungeonOfAbyss.Q00935_ExploringTheEastWingOfTheDungeonOfAbyss;
 import quests.Q10673_SagaOfLegend.Q10673_SagaOfLegend;
-import quests.Q10866_PunitiveOperationOnTheDevilIsle.Q10866_PunitiveOperationOnTheDevilIsle;
 import quests.Q10957_TheLifeOfADeathKnight.Q10957_TheLifeOfADeathKnight;
 import quests.Q10958_ExploringNewOpportunities.Q10958_ExploringNewOpportunities;
 import quests.Q10959_ChallengingYourDestiny.Q10959_ChallengingYourDestiny;
 import quests.Q10961_EffectiveTraining.Q10961_EffectiveTraining;
 import quests.Q10962_NewHorizons.Q10962_NewHorizons;
-import quests.Q10963_ExploringTheAntNest.Q10963_ExploringTheAntNest;
 import quests.Q10964_SecretGarden.Q10964_SecretGarden;
 import quests.Q10965_DeathMysteries.Q10965_DeathMysteries;
 import quests.Q10966_ATripBegins.Q10966_ATripBegins;
@@ -62,22 +43,17 @@ import quests.Q10987_PlunderedGraves.Q10987_PlunderedGraves;
 import quests.Q10988_Conspiracy.Q10988_Conspiracy;
 import quests.Q10989_DangerousPredators.Q10989_DangerousPredators;
 import quests.Q10990_PoisonExtraction.Q10990_PoisonExtraction;
-import quests.not_done.Q00630_PirateTreasureHunt;
-import quests.not_done.Q00664_QuarrelsTime;
-import quests.not_done.Q00910_RequestFromTheRedLibraGuildLv1;
-import quests.not_done.Q00911_RequestFromTheRedLibraGuildLv2;
-import quests.not_done.Q00912_RequestFromTheRedLibraGuildLv3;
-import quests.not_done.Q00913_RequestFromTheRedLibraGuildLv4;
-import quests.not_done.Q00914_RequestFromTheRedLibraGuildLv5;
-import quests.not_done.Q10861_MonsterArenaTheBirthOfAWarrior;
-import quests.not_done.Q10862_MonsterArenaChallenge10Battles;
-import quests.not_done.Q10863_MonsterArenaNewChallenge15Battles;
-import quests.not_done.Q10864_MonsterArenaBraveWarrior25Battles;
-import quests.not_done.Q10865_MonsterArenaLastCall40Battles;
-import quests.not_done.Q10867_GoneMissing;
-import quests.not_done.Q10868_TheDarkSideOfPower;
-import quests.not_done.Q10870_UnfinishedDevice;
-import quests.not_done.Q10871_DeathToThePirateKing;
+import quests.not_done.Q10968_ThePowerOfTheMagicLamp;
+import quests.not_done.Q10969_SporeInfestedPlace;
+import quests.not_done.Q10970_RespectForGraves;
+import quests.not_done.Q10971_TalismanEnchant;
+import quests.not_done.Q10972_CombiningGems;
+import quests.not_done.Q10973_EnchantingAgathions;
+import quests.not_done.Q10974_NewStylishEquipment;
+import quests.not_done.Q10975_LetsPayRespectsToOurFallenBrethren;
+import quests.not_done.Q10976_MemoryOfTheGloriousPast;
+import quests.not_done.Q10977_TracesOfBattle;
+import quests.not_done.Q10978_MissingPets;
 
 /**
  * @author NosBit
@@ -90,49 +66,14 @@ public class QuestMasterHandler
 	{
 		Q00127_FishingSpecialistsRequest.class,
 		Q00255_Tutorial.class,
-		Q00300_HuntingLetoLizardman.class,
-		Q00326_VanquishRemnants.class,
-		Q00327_RecoverTheFarmland.class,
-		Q00328_SenseForBusiness.class,
-		Q00329_CuriosityOfADwarf.class,
-		Q00331_ArrowOfVengeance.class,
-		Q00333_HuntOfTheBlackLion.class,
-		Q00344_1000YearsTheEndOfLamentation.class,
-		Q00354_ConquestOfAlligatorIsland.class,
-		Q00355_FamilyHonor.class,
-		Q00356_DigUpTheSeaOfSpores.class,
-		Q00358_IllegitimateChildOfTheGoddess.class,
-		Q00360_PlunderTheirSupplies.class,
-		Q00369_CollectorOfJewels.class,
-		Q00370_AnElderSowsSeeds.class,
-		Q00500_BrothersBoundInChains.class,
-		Q00630_PirateTreasureHunt.class, // TODO: Not done.
+		Q00502_BrothersBoundInChains.class,
 		Q00662_AGameOfCards.class,
-		Q00664_QuarrelsTime.class, // TODO: Not done.
-		Q00910_RequestFromTheRedLibraGuildLv1.class, // TODO: Not done.
-		Q00911_RequestFromTheRedLibraGuildLv2.class, // TODO: Not done.
-		Q00912_RequestFromTheRedLibraGuildLv3.class, // TODO: Not done.
-		Q00913_RequestFromTheRedLibraGuildLv4.class, // TODO: Not done.
-		Q00914_RequestFromTheRedLibraGuildLv5.class, // TODO: Not done.
-		Q00933_ExploringTheWestWingOfTheDungeonOfAbyss.class,
-		Q00935_ExploringTheEastWingOfTheDungeonOfAbyss.class,
 		Q10673_SagaOfLegend.class,
-		Q10861_MonsterArenaTheBirthOfAWarrior.class, // TODO: Not done.
-		Q10862_MonsterArenaChallenge10Battles.class, // TODO: Not done.
-		Q10863_MonsterArenaNewChallenge15Battles.class, // TODO: Not done.
-		Q10864_MonsterArenaBraveWarrior25Battles.class, // TODO: Not done.
-		Q10865_MonsterArenaLastCall40Battles.class, // TODO: Not done.
-		Q10866_PunitiveOperationOnTheDevilIsle.class,
-		Q10867_GoneMissing.class, // TODO: Not done.
-		Q10868_TheDarkSideOfPower.class, // TODO: Not done.
-		Q10870_UnfinishedDevice.class, // TODO: Not done.
-		Q10871_DeathToThePirateKing.class, // TODO: Not done.
 		Q10957_TheLifeOfADeathKnight.class,
 		Q10958_ExploringNewOpportunities.class,
 		Q10959_ChallengingYourDestiny.class,
 		Q10961_EffectiveTraining.class,
 		Q10962_NewHorizons.class,
-		Q10963_ExploringTheAntNest.class,
 		Q10964_SecretGarden.class,
 		Q10965_DeathMysteries.class,
 		Q10966_ATripBegins.class,
@@ -147,6 +88,17 @@ public class QuestMasterHandler
 		Q10988_Conspiracy.class,
 		Q10989_DangerousPredators.class,
 		Q10990_PoisonExtraction.class,
+		Q10968_ThePowerOfTheMagicLamp.class, // TODO: Not done.
+		Q10969_SporeInfestedPlace.class, // TODO: Not done.
+		Q10970_RespectForGraves.class, // TODO: Not done.
+		Q10971_TalismanEnchant.class, // TODO: Not done.
+		Q10972_CombiningGems.class, // TODO: Not done.
+		Q10973_EnchantingAgathions.class, // TODO: Not done.
+		Q10974_NewStylishEquipment.class, // TODO: Not done.
+		Q10975_LetsPayRespectsToOurFallenBrethren.class, // TODO: Not done.
+		Q10976_MemoryOfTheGloriousPast.class, // TODO: Not done.
+		Q10977_TracesOfBattle.class, // TODO: Not done.
+		Q10978_MissingPets.class, // TODO: Not done.
 	};
 	
 	public static void main(String[] args)
