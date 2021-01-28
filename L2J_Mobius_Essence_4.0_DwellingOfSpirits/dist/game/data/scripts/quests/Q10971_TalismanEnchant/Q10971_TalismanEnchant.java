031384009f00236618bac96fcf223e4d44292e95
MobiusDevelopment <MobiusDevelopment@7325c9f8-25fd-504a-9f63-8876acdc129b>
2021-01-28 20:04:45 +0000

    Addition of quests 10971, 10972 and 10973. Contributed by quangnguyen.

---
+++ L2J_Mobius_Essence_4.0_DwellingOfSpirits/dist/game/data/scripts/quests/Q10971_TalismanEnchant/Q10971_TalismanEnchant.java
@@ -0,0 +1,117 @@
+/*
+ * This file is part of the L2J Mobius project.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package quests.Q10971_TalismanEnchant;
+
+import org.l2jmobius.gameserver.model.actor.Npc;
+import org.l2jmobius.gameserver.model.actor.instance.PlayerInstance;
+import org.l2jmobius.gameserver.model.items.instance.ItemInstance;
+import org.l2jmobius.gameserver.model.quest.Quest;
+import org.l2jmobius.gameserver.model.quest.QuestState;
+import org.l2jmobius.gameserver.network.NpcStringId;
+import org.l2jmobius.gameserver.network.serverpackets.ExTutorialShowId;
+
+/**
+ * @author Mobius, quangnguyen
+ */
+public class Q10971_TalismanEnchant extends Quest
+{
+	// NPC
+	private static final int CAPTAIN_BATHIS = 30332;
+	// Item
+	private static final int TALISMAN_OF_ADEN = 91745;
+	// Misc
+	private static final int MIN_LEVEL = 25;
+	
+	public Q10971_TalismanEnchant()
+	{
+		super(10971);
+		addStartNpc(CAPTAIN_BATHIS);
+		addTalkId(CAPTAIN_BATHIS);
+		addCondMinLevel(MIN_LEVEL, "no_lvl.html");
+		setQuestNameNpcStringId(NpcStringId.LV_25_ENCHANT_TALISMAN);
+	}
+	
+	@Override
+	public String onAdvEvent(String event, Npc npc, PlayerInstance player)
+	{
+		final QuestState qs = getQuestState(player, false);
+		if (qs == null)
+		{
+			return null;
+		}
+		
+		String htmltext = null;
+		switch (event)
+		{
+			case "30332.htm":
+			case "30332-00.html":
+			case "30332-01.htm":
+			case "30332-02.htm":
+			{
+				htmltext = event;
+				break;
+			}
+			case "30332-03.htm":
+			{
+				qs.startQuest();
+				player.sendPacket(new ExTutorialShowId(47));
+				htmltext = event;
+				break;
+			}
+		}
+		return htmltext;
+	}
+	
+	@Override
+	public String onTalk(Npc npc, PlayerInstance player)
+	{
+		final QuestState qs = getQuestState(player, true);
+		String htmltext = getNoQuestMsg(player);
+		if (qs.isCreated())
+		{
+			htmltext = "30332.htm";
+		}
+		else if (qs.isStarted())
+		{
+			boolean foundEnchant = false;
+			SEARCH: for (ItemInstance item : player.getInventory().getItemsByItemId(TALISMAN_OF_ADEN))
+			{
+				if (item.getEnchantLevel() > 0)
+				{
+					foundEnchant = true;
+					break SEARCH;
+				}
+			}
+			if (foundEnchant)
+			{
+				addExpAndSp(player, 100000, 0);
+				qs.exitQuest(false, true);
+				htmltext = "30332-04.html";
+			}
+			else
+			{
+				htmltext = "30332-03.htm";
+				player.sendPacket(new ExTutorialShowId(47));
+			}
+		}
+		else if (qs.isCompleted())
+		{
+			htmltext = getAlreadyCompletedMsg(player);
+		}
+		return htmltext;
+	}
+}
--- L2J_Mobius_Essence_4.0_DwellingOfSpirits/dist/game/data/scripts/quests/QuestMasterHandler.java
+++ L2J_Mobius_Essence_4.0_DwellingOfSpirits/dist/game/data/scripts/quests/QuestMasterHandler.java
@@ -33,6 +33,9 @@ import quests.Q10964_SecretGarden.Q10964_SecretGarden;
 import quests.Q10965_DeathMysteries.Q10965_DeathMysteries;
 import quests.Q10966_ATripBegins.Q10966_ATripBegins;
 import quests.Q10967_CulturedAdventurer.Q10967_CulturedAdventurer;
+import quests.Q10971_TalismanEnchant.Q10971_TalismanEnchant;
+import quests.Q10972_CombiningGems.Q10972_CombiningGems;
+import quests.Q10973_EnchantingAgathions.Q10973_EnchantingAgathions;
 import quests.Q10981_UnbearableWolvesHowling.Q10981_UnbearableWolvesHowling;
 import quests.Q10982_SpiderHunt.Q10982_SpiderHunt;
 import quests.Q10983_TroubledForest.Q10983_TroubledForest;
@@ -46,9 +49,6 @@ import quests.Q10990_PoisonExtraction.Q10990_PoisonExtraction;
 import quests.not_done.Q10968_ThePowerOfTheMagicLamp;
 import quests.not_done.Q10969_SporeInfestedPlace;
 import quests.not_done.Q10970_RespectForGraves;
-import quests.not_done.Q10971_TalismanEnchant;
-import quests.not_done.Q10972_CombiningGems;
-import quests.not_done.Q10973_EnchantingAgathions;
 import quests.not_done.Q10974_NewStylishEquipment;
 import quests.not_done.Q10975_LetsPayRespectsToOurFallenBrethren;
 import quests.not_done.Q10976_MemoryOfTheGloriousPast;
@@ -91,9 +91,9 @@ public class QuestMasterHandler
 		Q10968_ThePowerOfTheMagicLamp.class, // TODO: Not done.
 		Q10969_SporeInfestedPlace.class, // TODO: Not done.
 		Q10970_RespectForGraves.class, // TODO: Not done.
-		Q10971_TalismanEnchant.class, // TODO: Not done.
-		Q10972_CombiningGems.class, // TODO: Not done.
-		Q10973_EnchantingAgathions.class, // TODO: Not done.
+		Q10971_TalismanEnchant.class,
+		Q10972_CombiningGems.class,
+		Q10973_EnchantingAgathions.class,
 		Q10974_NewStylishEquipment.class, // TODO: Not done.
 		Q10975_LetsPayRespectsToOurFallenBrethren.class, // TODO: Not done.
 		Q10976_MemoryOfTheGloriousPast.class, // TODO: Not done.
