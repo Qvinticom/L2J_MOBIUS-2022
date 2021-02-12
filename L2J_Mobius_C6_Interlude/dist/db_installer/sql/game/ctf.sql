76fa2663e07479308311163c1ce5ee07be88060f
MobiusDevelopment <MobiusDevelopment@7325c9f8-25fd-504a-9f63-8876acdc129b>
2021-02-12 05:25:44 +0000

    Proper level naming for event SQL tables. Thanks to privetdruk.

--- L2J_Mobius_C6_Interlude/dist/db_installer/sql/game/ctf.sql
+++ L2J_Mobius_C6_Interlude/dist/db_installer/sql/game/ctf.sql
@@ -7,8 +7,8 @@ CREATE TABLE `ctf` (
   `eventName` varchar(255) NOT NULL default '',
   `eventDesc` varchar(255) NOT NULL default '',
   `joiningLocation` varchar(255) NOT NULL default '',
-  `minlvl` int(4) NOT NULL default '0',
-  `maxlvl` int(4) NOT NULL default '0',
+  `minLevel` int(4) NOT NULL default '0',
+  `maxLevel` int(4) NOT NULL default '0',
   `npcId` int(8) NOT NULL default '0',
   `npcX` int(11) NOT NULL default '0',
   `npcY` int(11) NOT NULL default '0',
@@ -24,5 +24,5 @@ CREATE TABLE `ctf` (
   `delayForNextEvent` BIGINT NOT NULL DEFAULT '0'
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-INSERT INTO `ctf` (`eventName`, `eventDesc`, `joiningLocation`, `minlvl`, `maxlvl`, `npcId`, `npcX`, `npcY`, `npcZ`, `npcHeading`, `rewardId`, `rewardAmount`, `teamsCount`, `joinTime`, `eventTime`, `minPlayers`, `maxPlayers`, `delayForNextEvent`) VALUES
+INSERT INTO `ctf` (`eventName`, `eventDesc`, `joiningLocation`, `minLevel`, `maxLevel`, `npcId`, `npcX`, `npcY`, `npcZ`, `npcHeading`, `rewardId`, `rewardAmount`, `teamsCount`, `joinTime`, `eventTime`, `minPlayers`, `maxPlayers`, `delayForNextEvent`) VALUES
 ('Capture the flag', 'CTF', 'Giran', 1, 80, 70011, 82580, 148552, -3468, 16972, 8752, 1, 2, 5, 5, 2, 50, 300000);
