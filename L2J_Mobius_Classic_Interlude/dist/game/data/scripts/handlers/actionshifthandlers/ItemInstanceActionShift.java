1f8a0b193d4c0aecfe4ae2be4b46849592138833
MobiusDevelopment <MobiusDevelopment@7325c9f8-25fd-504a-9f63-8876acdc129b>
2021-10-25 21:40:41 +0000

    Show name as title for admin item action shift menu.

--- L2J_Mobius_Classic_Interlude/dist/game/data/scripts/handlers/actionshifthandlers/ItemInstanceActionShift.java
+++ L2J_Mobius_Classic_Interlude/dist/game/data/scripts/handlers/actionshifthandlers/ItemInstanceActionShift.java
@@ -30,7 +30,7 @@ public class ItemInstanceActionShift implements IActionShiftHandler
 	{
 		if (player.isGM())
 		{
-			final NpcHtmlMessage html = new NpcHtmlMessage(0, 1, "<html><body><center><font color=\"LEVEL\">Item Info</font></center><br><table border=0><tr><td>Object ID: </td><td>" + target.getObjectId() + "</td></tr><tr><td>Item ID: </td><td>" + target.getId() + "</td></tr><tr><td>Owner ID: </td><td>" + ((ItemInstance) target).getOwnerId() + "</td></tr><tr><td>Location: </td><td>" + target.getLocation() + "</td></tr><tr><td><br></td></tr><tr><td>Class: </td><td>" + target.getClass().getSimpleName() + "</td></tr></table></body></html>");
+			final NpcHtmlMessage html = new NpcHtmlMessage(0, 1, "<html><head><title>" + target.getName() + "</title></head><body><center><font color=\"LEVEL\">Item Info</font></center><br><table border=0><tr><td>Object ID: </td><td>" + target.getObjectId() + "</td></tr><tr><td>Item ID: </td><td>" + target.getId() + "</td></tr><tr><td>Owner ID: </td><td>" + ((ItemInstance) target).getOwnerId() + "</td></tr><tr><td>Location: </td><td>" + target.getLocation() + "</td></tr><tr><td><br></td></tr><tr><td>Class: </td><td>" + target.getClass().getSimpleName() + "</td></tr></table></body></html>");
 			player.sendPacket(html);
 		}
 		return true;
